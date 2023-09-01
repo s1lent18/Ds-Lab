28e8586a864960206c4ae82c646e92820a40a391
s11ent18 <alijafar.rajani@gmail.com>
2023-09-01 21:48:30 +0500

    Incomplete Q6

--- Lab-2/hometaskQ-6.cpp
+++ Lab-2/hometaskQ-6.cpp
@@ -2,3 +2,29 @@
 #include <string>
 using namespace std;
 
+class Time
+{
+    public:
+        int seconds;
+        int mins;
+        int hrs;
+
+        void Sort(Time * ptr, int size)
+        {
+            for(int i = 0; i < size - 1; i++)
+            {
+                int minindex= i;
+
+                for(int j = i + 1; j < size; j++)
+                {
+                    minindex = j;
+                }
+                if(minindex != i)
+                {
+                    swap(ptr[i], ptr[minindex]);
+                }
+            }
+
+            
+        }
+};
