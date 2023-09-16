8c8a491feec08f39a7cee449c4bb2cc8c104a543
s11ent18 <alijafar.rajani@gmail.com>
2023-09-16 19:10:00 +0500

    Q-2

---
+++ Lab-4/Home-Tasks/Q-2.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node * next;
+        Node * prev;
+        int key;
+
+        Node()
+        {
+
+        };
+
+        Node(int value)
+        {
+            data = value;
+            next = NULL;
+            prev = NULL;
+        }
+
+};
+
+class DoublyLinkedList
+{
+    public:
+        Node * head;
+
+        void insertathead(int value)
+        {
+            Node * n = new Node(value);
+
+            if(head == NULL)
+            {
+                n->key = 0;
+                
+                head = n;
+
+                return;
+            }
+
+            Node * temp = head;
+
+            while(temp != NULL)
+            {
+                temp->key = temp->key + 1;
+
+                temp = temp->next;   
+            }
+            
+            head->prev = n;
+
+            n->next = head;
+
+            n->key = 0;
+
+            head = n;
+        }
+
+        void insertattail(int value)
+        {
+            Node * n = new Node(value);
+
+            Node * temp = head;
+
+            if(head == NULL)
+            {
+                head = n;
+
+                n->key = 0;
+
+                return;
+            }
+
+            while(temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+
+            temp->next = n;
+
+            n->prev = temp;
+
+            n->key = temp->key + 1;
+        }
+
+        void insertatindex(int value, int index)
+        {
+            
+        }
+
+        void displayforward()
+        {
+            Node * temp = head;
+
+            cout << endl;
+
+            while(temp != NULL)
+            {
+                cout << temp->data << "->";
+
+                temp = temp->next;
+            }
+
+            cout << "NULL" << endl << endl;
+
+            temp = head;
+
+            while(temp != NULL)
+            {
+                cout << temp->key << ", ";
+
+                temp = temp->next;
+            }
+        }
+};
+
+int main()
+{
+    DoublyLinkedList * D = new DoublyLinkedList();
+
+    D->insertattail(1);
+    D->insertattail(2);
+    D->insertattail(3);
+    D->insertattail(4);
+    D->insertattail(5);
+
+    D->insertathead(0);
+
+    D->displayforward();
+}
