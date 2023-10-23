599c43b9e1e2157c1159b01404d5b4c87764641e
s1lent18 <alijafar.rajani@gmail.com>
2023-10-23 19:50:03 +0500

    Q-2

--- Lab-7/Home-Tasks/Q-2.cpp
+++ Lab-7/Home-Tasks/Q-2.cpp
@@ -51,21 +51,150 @@ class LinkedList
             temp->next = n;
         }
 
-        void deleteathead()
+        Node * Top()
+        {
+            return head;
+        }
+
+        Node * deleteathead()
         {
             Node * del = head;
 
             head = head->next;
 
-            del = NULL;
+            del->next = NULL;
 
-            delete del;
+            return del;
         }
 
 
 };
 
+class Queue
+{
+    public:
+        LinkedList * arr = new LinkedList();
+
+        void push(int taskid, int storage, int time, int memory)
+        {
+            arr->insertattail(taskid, storage, time, memory);
+        }   
+
+        Node * pop()
+        {
+            return arr->deleteathead();
+        }
+
+        Node * peek()
+        {
+            return arr->Top();
+        }    
+
+        Node * minmemory()
+        {
+            Node * temp = arr->head;
+
+            Node * ret = new Node();
+
+            int min = 1024;
+            int id = 0;
+
+            while(temp != NULL)
+            {
+                if(min > temp->memory)
+                {
+                    min = temp->memory;
+                    ret = temp;
+                }
+                temp = temp->next;
+            }
+
+            return ret;
+        }
+
+        void deletenode(Node * n)
+        {
+            Node * temp = arr->head;
+
+            while(temp != NULL)
+            {
+                if(n->taskid == temp->next->taskid)
+                {
+                    Node * del = temp->next;
+
+                    temp->next = temp->next->next;
+
+                    del = NULL;
+
+                    delete del;
+                }
+
+                temp = temp->next;
+            }
+        }
+
+        bool isempty()
+        {
+            return arr->head == NULL;
+        }
+        
+};
+
+void Q2()
+{
+    Queue Q1;
+    Queue Q2;
+
+    Q1.push(1, 2, 20, 512);
+    Q1.push(2, 1, 15, 256);
+    Q1.push(3, 3, 25, 768);
+    Q1.push(4, 2, 18, 384);
+
+    int size = 1024;
+
+    while(!Q1.isempty() && !Q2.isempty())
+    {
+        Node * p = Q1.minmemory();
+
+        int taskid = p->taskid;
+        int storage = p->storage;
+        int time = p->time;
+        int memory = p->memory;
+
+        if(size > p->memory)
+        {
+            size -= p->memory;
+
+            Q2.push(taskid, storage, time, memory);
+        }
+        else
+        {
+            Node * check = Q2.pop();
+
+            taskid = check->taskid;
+            storage = check->storage;
+            time = check->time;
+            memory = check->memory;
+
+            Q1.push(taskid, storage, time, memory);
+        }
+        
+        Q1.deletenode(p);
+
+        if(Q1.isempty())
+        {
+            while(!Q2.isempty())
+            {
+                Q2.pop();
+            }
+        }
+    }
+
+}
+
 int main()
 {
+    Q2();
+
     return 0;
 }
