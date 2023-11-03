0d0787fabf3e9d943b0c0f17274a2e3d7a1c6b02
s1lent18 <alijafar.rajani@gmail.com>
2023-11-03 11:36:35 +0500

    Done

--- Lab-8/Home-Tasks/Q-2.cpp
+++ Lab-8/Home-Tasks/Q-2.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-class Node
+class Node 
 {
     public:
         int data;
         Node * left;
         Node * right;
 
-        Node()
+        Node() 
         {
 
         }
 
-        Node(int data)
+        Node(int data) 
         {
             this->data = data;
             left = NULL;
@@ -21,128 +21,168 @@ class Node
         }
 };
 
-class BST
+class BST 
 {
     public:
         Node * root;
 
-        Node * insert(int data, Node *& head)
+        Node * insert(int data, Node*& head) 
         {
-            if(head == NULL)
+            if (head == NULL) 
             {
-                Node * b = new Node(data);
+                Node* newNode = new Node(data);
 
-                head = b;
-            }
-            else if(data <= head->data)
+                head = newNode;
+
+            } else if (data < head->data) 
             {
                 head->left = insert(data, head->left);
-            }
-            else if(data >= head->data)
+
+            } else if (data > head->data) 
             {
                 head->right = insert(data, head->right);
+
             }
             return head;
         }
 
-        Node * deleteallduplicates(Node *& head)
+        Node * Min(Node* head) 
         {
-            if(head == NULL)
+            if (head == NULL) 
+            {
+                return NULL;
+
+            } else if (head->left == NULL) 
+            {
+                return head;
+
+            } else 
+            {
+                return Min(head->left);
+            }
+        }
+
+        Node * deletebyvalue(Node*& head, int data) 
+        {
+            if (head == NULL) 
             {
                 return NULL;
             }
-
-            head->left = deleteallduplicates(head->left);
-            head->right = deleteallduplicates(head->right);
-
-            if(head->left != NULL)
+            if (data < head->data) 
             {
-                if(head->left->data == head->data)
-                {
-                    Node * temp = head->left;
+                head->left = deletebyvalue(head->left, data);
 
-                    head = NULL;
-
-                    delete head;
-
-                    return temp;
-                }
-            }
-
-            if(head->right != NULL)
+            } else if (data > head->data) 
             {
-                if(head->right->data == head->data)
+                head->right = deletebyvalue(head->right, data);
+
+            } else 
+            {
+                if (head->left == NULL) 
                 {
                     Node * temp = head->right;
 
-                    head = NULL;
+                    delete head;
+
+                    return temp;
+
+                } else if (head->right == NULL) 
+                {
+                    Node * temp = head->left;
 
                     delete head;
 
                     return temp;
                 }
-            }
 
+                Node * temp = Min(head->right);
+
+                head->data = temp->data;
+
+                head->right = deletebyvalue(head->right, temp->data);
+            }
             return head;
         }
 
-        Node * search(Node *& head, int data)
+        void S(string a, int d, Node*& head) 
         {
-            if(head == NULL)
+            bool ai = false;
+            bool bi = false;
+
+            if (a == "ceil") 
             {
-                return NULL;
-            }
-            else if(data < head->data)
+                ai = search(head, d + 1);
+
+            } else if (a == "floor") 
             {
-                return search(head->right, data);
+                bi = search(head, d - 1);
             }
-            else if(data > head->data)
+
+            if (ai) 
+            {
+                deletebyvalue(head, d + 1);
+
+            } else if (bi) 
+            {
+                deletebyvalue(head, d - 1);
+            }
+        }
+
+        bool search(Node *& head, int data) 
+        {
+            if (head == NULL) 
+            {
+                return false;
+
+            } else if (data < head->data) 
             {
                 return search(head->left, data);
-            }
-            else
-            {
-                return head;
-            }
-        }  
 
-        
+            } else if (data > head->data) 
+            {
+                return search(head->right, data);
+
+            } else if (data == head->data) 
+            {
+                return true;
+            }
+            return false;
+        }
 };
 
-void preorder(Node * root)
+void preorder(Node * root) 
 {
-    if(root == NULL)
+    if (root == NULL) 
     {
-        return; 
+        return;
     }
 
     cout << root->data << " ";
-
     preorder(root->left);
     preorder(root->right);
 }
 
-int main()
+int main() 
 {
     BST * B = new BST();
 
     B->insert(54, B->root);
     B->insert(23, B->root);
-    B->insert(10, B->root);
     B->insert(67, B->root);
     B->insert(10, B->root);
     B->insert(40, B->root);
     B->insert(90, B->root);
-    B->insert(40, B->root);
     B->insert(2, B->root);
-    B->insert(23, B->root);
 
     preorder(B->root);
-
-    B->deleteallduplicates(B->root);
-
+    
     cout << endl;
 
+    B->search(B->root, 23);
+
+    B->S("ceil", 22, B->root);
+
     preorder(B->root);
 
+    return 0;
 }
